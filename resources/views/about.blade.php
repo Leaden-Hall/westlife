441464fc91396786343b9c5175fb329ee4ad73e4
= <=>
2018-04-28 17:00:46 +0700

    add discography section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -37,74 +37,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-4 col-md-push-1">
-                    <aside class="sidebar">
-                        <div class="widget">
-                            <h3 class="widget-title">Discography</h3>
-                            <ul class="discography-list">
-                                <li>
-                                    <figure class="cover">
-                                        <img src="images/thumbnail-1.jpg" alt="thumbnail 1">
-                                    </figure>
-
-                                    <div class="detail">
-                                        <h3><a href="#">Deserunt mollitia animi</a></h3>
-                                        <span class="year">2004</span>
-                                        <span class="track">17 tracks</span>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <figure class="cover">
-                                        <img src="images/thumbnail-2.jpg" alt="thumbnail 2">
-                                    </figure>
-
-                                    <div class="detail">
-                                        <h3><a href="#">Deserunt mollitia animi</a></h3>
-                                        <span class="year">2004</span>
-                                        <span class="track">17 tracks</span>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <figure class="cover">
-                                        <img src="images/thumbnail-3.jpg" alt="thumbnail 3">
-                                    </figure>
-
-                                    <div class="detail">
-                                        <h3><a href="#">Deserunt mollitia animi</a></h3>
-                                        <span class="year">2004</span>
-                                        <span class="track">17 tracks</span>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <figure class="cover">
-                                        <img src="images/thumbnail-4.jpg" alt="thumbnail 4">
-                                    </figure>
-
-                                    <div class="detail">
-                                        <h3><a href="#">Deserunt mollitia animi</a></h3>
-                                        <span class="year">2004</span>
-                                        <span class="track">17 tracks</span>
-                                    </div>
-                                </li>
-
-                                <li>
-                                    <figure class="cover">
-                                        <img src="images/thumbnail-5.jpg" alt="thumbnail 5">
-                                    </figure>
-
-                                    <div class="detail">
-                                        <h3><a href="#">Deserunt mollitia animi</a></h3>
-                                        <span class="year">2004</span>
-                                        <span class="track">17 tracks</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
-                </div>
+                @include('discography')
             </div>
         </div>
     </section>
